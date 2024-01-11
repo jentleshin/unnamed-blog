bcb7252e8c0772755145da99a103070227c43a95
jentleshin <jentleshin@gmail.com>
2024-01-11 09:35:54 +0900

    update text

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -9,19 +9,22 @@ interface IProps {
     className?: string;
     title?: boolean;
     subtitle?: boolean;
+    plaintitle?: boolean;
+    texttitle?: boolean;
     p?: boolean;
+    token?: boolean;
     quote?: boolean;
     id?: string;
 }
 
-const Text = ({ children, color, textAlign = TextAlign.NONE, className, title = false, subtitle = false, p = false, quote = false, id }: IProps) => {
+const Text = ({ children, color, textAlign = TextAlign.NONE, className, title = false, subtitle = false, plaintitle = false, texttitle = false, p = false, token = false, quote = false, id }: IProps) => {
     const computeComponent = () => {
         let ui = <></>;
 
         if (title) {
             ui = (
                 <h1
-                    className={combineClasses(`font-bold text-3xl md:text-4xl my-[10px]`, className)} style={{ color: color, textAlign: textAlign }} id={id}>
+                    className={combineClasses(`font-display text-3xl md:text-6xl py-3`, className)} style={{ color: color, textAlign: textAlign }} id={id}>
                     {children}
                     
                 </h1>
@@ -29,15 +32,39 @@ const Text = ({ children, color, textAlign = TextAlign.NONE, className, title = 
         } else if (subtitle) {
             ui = (
                 <h2
-                    className={combineClasses(`font-semibold text-xl md:text-2xl my-[10px]`, className)}
+                    className={combineClasses(`font-display text-xl md:text-3xl py-3`, className)}
                     style={{ color: color, textAlign: textAlign }} id={id}>
                     {children}
                 </h2>
             )
+        } else if (plaintitle) {
+            ui = (
+                <h3
+                    className={combineClasses(`font-text font-semibold text-xl md:text-3xl py-4`, className)}
+                    style={{ color: color, textAlign: textAlign }} id={id}>
+                    {children}
+                </h3>
+            )
+        } else if (texttitle) {
+            ui = (
+                <p
+                    className={combineClasses(`font-text font-semibold md:text-xl text-sm md:leading-8 leading-relaxed py-3`, className)}
+                    style={{ color: color, textAlign: textAlign }} id={id}>
+                    {children}
+                </p>
+            )
         } else if (p) {
             ui = (
                 <p
-                    className={combineClasses(`font-regular mb-3 text-lg leading-relaxed`, className)}
+                    className={combineClasses(`font-text md:text-base text-sm md:leading-8 leading-relaxed py-2`, className)}
+                    style={{ color: color, textAlign: textAlign }} id={id}>
+                    {children}
+                </p>
+            )
+        } else if (token) {
+            ui = (
+                <p
+                    className={combineClasses(`font-text md:text-sm text-sm py-1 opacity-60 uppercase`, className)}
                     style={{ color: color, textAlign: textAlign }} id={id}>
                     {children}
                 </p>
@@ -53,7 +80,7 @@ const Text = ({ children, color, textAlign = TextAlign.NONE, className, title = 
         } else {
             ui = (
                 <p
-                    className={combineClasses(`font-regular mb-3 text-lg leading-relaxed`, className)}
+                    className={combineClasses(`font-text mb-3 text-lg leading-relaxed`, className)}
                     style={{ color: color, textAlign: textAlign }} id={id}>
                     {children}
                 </p>
