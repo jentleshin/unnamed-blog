14fc6e625548cbbf5d9757664d4d77cbddfb8c4d
jentleshin <jentleshin@gmail.com>
2024-01-11 09:40:08 +0900

    Seperator

--- src/components/Seperator/index.tsx
+++ src/components/Seperator/index.tsx
@@ -4,21 +4,22 @@ import classes from './Seperator.module.scss';
 interface iProps {
     line?: boolean;
     dots?: boolean;
+    className?: string;
 }
 
-const Seperator = ({ line = false, dots = false }: iProps) => {
+const Seperator = ({ line = false, dots = false, className }: iProps) => {
     return (
         <>
             {
-                line ? <div className={combineClasses(classes.section_seperator_line, 'dark:border-white border-black')}></div> :
-                    dots ? <div className={combineClasses(classes.section_seperator_dots)}>
+                line ? <div className={combineClasses(classes.section_seperator_line, 'dark:border-lime border-organic', className)}></div> :
+                    dots ? <div className={combineClasses(classes.section_seperator_dots, className)}>
                         {
                             [...Array(5)].map((i) => (
                                 <span key={Math.random()} className="dark:bg-white"></span>
                             ))
                         }
                     </div> : 
-                    <div className={combineClasses(classes.section_seperator_line, 'dark:border-white border-black')}></div>
+                    <div className={combineClasses(classes.section_seperator_line, 'dark:border-lime border-organic', className)}></div>
             }
 
         </>
